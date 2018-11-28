b0df90aa2aaa65b2882d31d9d26498a4c3f07c44
Aykut Büyükkılıç <aykut.buyukkilic@gmail.com>
2018-11-28 11:12:30 +0300

    jp course4 add

---
+++ AdaYazilimAkademi/src/jp_04_foreach/ForEachTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp_04_foreach;
+
+/**
+ *
+ * @author bilsis
+ */
+public class ForEachTest {
+
+    public static void main(String[] args) {
+       
+        int[] ages = {15, 20, 41, 50};
+        for (int age : ages) {
+            System.out.println("Yaş :" + age);
+        }
+        
+        Person[] people = {new Person("Aykut"), new Person("Ada"), new Person("Dicle")};
+        
+        System.out.println("");
+        
+        for(Person p : people){
+            int personId = p.getPersonId();
+            String name = p.getName();
+            System.out.println("Kişi Id: "+ personId + " Adı = " + name);
+        }
+
+    }
+}
---
+++ AdaYazilimAkademi/src/jp_04_foreach/Person.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp_04_foreach;
+
+/**
+ *
+ * @author bilsis
+ */
+public class Person {
+    private final int personId;
+    private String name;
+    private static int peopleCounter;
+
+    public Person(String name) {
+        personId = ++peopleCounter;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getPersonId(){
+        return this.personId;
+    }
+    
+
+    
+    
+    
+    
+    
+}
